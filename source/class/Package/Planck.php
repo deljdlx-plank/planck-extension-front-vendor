525f2c44baa6baad5190487eab536a8114715ec3
Jean luc <you@example.com>
2019-02-07 12:29:33 +0100

    synchro

--- source/class/Package/Planck.php
+++ source/class/Package/Planck.php
@@ -17,13 +17,16 @@ class Planck extends Component
 
 
         $this->addJavascriptFile('vendor/planck-front/source/Planck.js', self::RESOURCE_PRIORITY_INCLUDE);
-        $this->addJavascriptFile('vendor/planck-front/source/Modal.js', self::RESOURCE_PRIORITY_INCLUDE);
+
         $this->addJavascriptFile('vendor/planck-front/source/Router.js', self::RESOURCE_PRIORITY_INCLUDE);
         $this->addJavascriptFile('vendor/planck-front/source/Controller.js', self::RESOURCE_PRIORITY_INCLUDE);
         $this->addJavascriptFile('vendor/planck-front/source/Application.js', self::RESOURCE_PRIORITY_INCLUDE);
 
         $this->addJavascriptFile('vendor/planck-front/source/Model/Repository.js', self::RESOURCE_PRIORITY_INCLUDE);
         $this->addJavascriptFile('vendor/planck-front/source/Model/Entity.js', self::RESOURCE_PRIORITY_INCLUDE);
+
+        $this->addJavascriptFile('vendor/planck-front/source/DataLayer.js', self::RESOURCE_PRIORITY_INCLUDE);
+        $this->addJavascriptFile('vendor/planck-front/source/DataLayerEntry.js', self::RESOURCE_PRIORITY_INCLUDE);
 
 
         $this->addJavascriptFile('vendor/planck-front/source/Component/FileUploader.js', self::RESOURCE_PRIORITY_INCLUDE);
